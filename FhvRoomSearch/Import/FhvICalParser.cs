ccd9b76c95833011d7327591af128ccd386a1c75
Danielku15 <Danielku15@CoderLine.net>
2011-11-07 20:20:03 +0100

    Created basic structure for ical parser

---
+++ FhvRoomSearch/Import/FhvICalParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DDay.iCal;
+using FhvRoomSearch.Model;
+
+namespace FhvRoomSearch.Import
+{
+    /// <summary>
+    /// This class is used to parse an iCal file into the internal room searcher model. 
+    /// </summary>
+    class FhvICalParser
+    {
+        private IICalendar _calendar;
+
+        public IList<Wing> ParsedData { get; private set; }
+
+        public FhvICalParser(IICalendar iICalendar)
+        {
+            _calendar = iICalendar;
+        }
+
+        public void Parse()
+        {
+            ParsedData = new List<Wing>();
+        }
+
+
+        public static FhvICalParser FromFile(string file, Encoding encoding)
+        {
+            try
+            {
+                IICalendarCollection collection = iCalendar.LoadFromFile(file, encoding);
+                if (collection.Count == 0)
+                {
+                    throw new ImportException("Could not find calendar data in the specified file.");
+                }
+
+                return new FhvICalParser(collection[0]);
+            }
+            catch (ImportException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new ImportException(string.Format("Error loading the calendar file: {0}", e.Message), e);
+            }
+        }
+    }
+}
---
+++ FhvRoomSearch/Import/ImportException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace FhvRoomSearch.Import
+{
+    [Serializable]
+    public class ImportException : Exception
+    {
+        public ImportException()
+        {
+        }
+
+        public ImportException(string message) : base(message)
+        {
+        }
+
+        public ImportException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected ImportException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
